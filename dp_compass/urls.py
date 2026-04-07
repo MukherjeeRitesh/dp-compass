d4646910919a4ababa4c9364e6b33aec090bf004
Ritesh Mukherjee <riteshmukherjee@Riteshs-MacBook-Pro.local>
2026-04-07 20:38:43 +0530

    Add temporary password reset view

--- dp_compass/urls.py
+++ dp_compass/urls.py
@@ -5,8 +5,31 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def reset_passwords_secret(request):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    users_to_reset = {
+        'admin': 'admin123',
+        'auditor1': 'auditor123',
+        'developer1': 'developer123'
+    }
+    msgs = []
+    for username, password in users_to_reset.items():
+        try:
+            u = User.objects.get(username=username)
+            u.set_password(password)
+            u.save()
+            msgs.append(f"Success: Reset password for {username}")
+        except User.DoesNotExist:
+            msgs.append(f"Error: User {username} does not exist.")
+        except Exception as e:
+            msgs.append(f"Error: {str(e)}")
+    return HttpResponse("<br>".join(msgs) + "<br><br><b>Done! Please do not forget to remove this code and re-deploy once you have logged in!</b>")
 
 urlpatterns = [
+    path('reset-default-passwords-secret-12345/', reset_passwords_secret),
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('users/', include('apps.users.urls')),
